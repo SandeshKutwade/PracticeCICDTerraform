9240cc40b10f2af9d82cfdb3896c390a5f3a1d38
sandesh <sandeshkutwade6@gmail.com>
2024-07-24 14:10:46 +0530

    added output.tf and providers.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-resource "aws_instance" "example" {
+resource "aws_instance" "CI" {
   ami           = "ami-0ad21ae1d0696ad58"  # Update with a valid AMI ID
   instance_type = "t2.micro"
 
