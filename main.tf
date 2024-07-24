3333b983c57b8fb849df03f9348d6588de8b720b
sandesh <sandeshkutwade6@gmail.com>
2024-07-24 09:57:31 +0530

    Add GitHub Actions workflow for Terraform

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0ad21ae1d0696ad58"  # Update with a valid AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
