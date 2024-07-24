24319bc3337d7b709ae0b13744025b46636ec5a8
sandesh <sandeshkutwade6@gmail.com>
2024-07-24 17:09:48 +0530

    removed state

--- main.tf
+++ main.tf
@@ -7,30 +7,3 @@ resource "aws_instance" "CITerraform" {
   }
 }
 
-resource "aws_s3_bucket" "StateBucket" {
-  bucket = "terraformstateitsminne"
-  tags = {
-    Name = "StateBucket"
-  }
-}
-resource "aws_dynamodb_table" "StateDynamoTable" {
-  name         = "statefile_table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-}
-
-terraform {
-  backend "s3" {
-    bucket = "terraformstateitsminne"
-    key = "terraform/state"
-    region = "ap-south-1"
-    encrypt = false
-    dynamodb_table = "statefile_table"
-  }
-}
-
